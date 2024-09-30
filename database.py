29cbcd09c935707f0b58b3f5fa5b2f80748d71f2
Dushyant Kumar <104586436+25007839d@users.noreply.github.com>
2024-09-30 20:31:00 +0530

    Revert "api updated with form"

--- database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-
-SQLALCHEMY_DB_URL = "sqlite:///./sql_app_db"
-
-engine = create_engine(SQLALCHEMY_DB_URL)
-SessionLocal = sessionmaker(autocommit=False,bind=engine)
-Base = declarative_base()
